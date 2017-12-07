d9f33a131f8e0db89b2b32aacbdee9ba8d3941f9
Zac McClung <zac.mcclung370@gmail.com>
2017-12-06 22:08:51 -0700

    added weather component

--- components/Landing.js
+++ components/Landing.js
@@ -3,10 +3,12 @@ import Time from './Time'
 import Quotes from './Quotes'
 import Greeting from './Greeting'
 import styles from '.././public/index.css'
+import Weather from './Weather'
 
 const Landing = () => {
   return (
     <div className='landing-main'>
+      <Weather />
       <h1>Hello World</h1>
       <p>Welcome to my world</p>
       <Time />
---
+++ components/Weather.js
@@ -0,0 +1,10 @@
+import React from 'React'
+
+const Weather = () => {
+  return (
+    <div className='weather-main'>
+      <p>73 &#8457;</p>
+    </div>
+  )
+}
+export default Weather
